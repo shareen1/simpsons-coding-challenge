25b34d241a278332659d17b45889ac39162ba8a5
vmaji <vmaji@DESKTOP-PEGQV09>
2020-03-12 15:48:04 +0000

    fixed cancel button in create page

--- simpsons-coding-challenge/src/main/resources/static/createch.js
+++ simpsons-coding-challenge/src/main/resources/static/createch.js
@@ -1,37 +1,43 @@
-$(document).ready(function() {
-	$("#id_form").submit(function(event){
-		event.preventDefault();
-		ajaxPost();
-		 });
-function ajaxPost(){
-	var formData = {
-			chId : $("#chId").val(),
-			 firstName : $("#firstName").val(),
-			 lastName : $("#lastName").val(),
-			 comments : [$("#comments").val()],
-			 age : $("#age").val()
-			}
-    $.ajax({
-        type: 'POST',
-        url: 'http://localhost:8083/postCharacter',
-        contentType: "application/json",
-        dataType: 'json',
-        data:JSON.stringify(formData),
-        success: function(result) {
-            console.log(result);
-            if(result.status == "success")
-            	{
-            	$("#postResultDiv").html(""+result.data.firstName+"Post Successfully");
-            	window.location.replace("http://localhost:8083");
-            	}else{
-            		$("#postResultDiv").html("<strong>ERROR</strong>");
-            	 	}
-        },
-            	 	error :function(e){
-            	 		alert("Error")
-            	 		 console.log("Error",e);
-            	 		}          
+$(document).ready(
+		function() {
+			$("#id_form").submit(function(event) {
+				event.preventDefault();
+				ajaxPost();
+			});
+			function ajaxPost() {
+				var formData = {
+					chId : $("#chId").val(),
+					firstName : $("#firstName").val(),
+					lastName : $("#lastName").val(),
+					comments : [ $("#comments").val() ],
+					age : $("#age").val()
+				}
+				$.ajax({
+					type : 'POST',
+					url : 'http://localhost:8083/postCharacter',
+					contentType : "application/json",
+					dataType : 'json',
+					data : JSON.stringify(formData),
+					success : function(result) {
+						console.log(result);
+						if (result.status == "success") {
+							$("#postResultDiv").html(
+									"" + result.data.firstName
+											+ "Post Successfully");
+							window.location.replace("http://localhost:8083");
+						} else {
+							$("#postResultDiv").html("<strong>ERROR</strong>");
+						}
+					},
+					error : function(e) {
+						alert("Error")
+						console.log("Error", e);
+					}
 
-    });
-}
-});
+				});
+			}
+			$('#cancel').on('click', 'button', function(event) {
+				window.location.replace("http://localhost:8083");
+			});
+
+		});
